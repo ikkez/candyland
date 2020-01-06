31870bcb9f3bf92b8e33bd956e2320baed0d3085
ikkez <ikkez0n3@gmail.com>
2020-01-06 06:55:37 +0100

    clockwork cookie check

--- log/clockwork/debugger.php
+++ log/clockwork/debugger.php
@@ -12,33 +12,44 @@ class Debugger extends \Prefab {
 
 	use ComponentTrait;
 
+	protected $enabled = false;
+	protected $clock;
+
 	function ready() {
 
-		if ($this->fw->exists('ENABLE_CLOCKWORK',$go) && !$go)
+		if (!isset($this->config['enable']) || !$this->config['enable'])
 			return;
 
 		$options = [
 			'register_helpers'     => true,
 			'enable'               => true,
-			'storage'              => 'files',
+			'check_cookie'         => 'XDEBUG_SESSION',
+			'storage'              => 'files', // sql
 			'storage_files_path'   => $this->fw->get('LOGS').'clockwork/',
-			//'storage'              => 'sql',
-			//			'storage_sql_database' => 'sqlite:data/clockwork.sqlite',
-			'storage_expiration' => 60*24*7, // minutes
-			'serialization_depth' => 3,
-			'auth_password' => '',
+			'storage_sql_database' => 'sqlite:data/clockwork.sqlite',
+			'storage_expiration'   => 60*24*7, // minutes
+			'serialization_depth'  => 3,
+			'auth_password'        => '',
 		];
 		$options = array_replace_recursive($options,$this->config);
+
+		if (!empty($options['check_cookie']) && !$this->fw->exists('COOKIE.'.$options['check_cookie'])) {
+			return;
+		}
+
+		$this->enabled = TRUE;
+
 		$clock = \Clockwork\Support\Vanilla\Clockwork::init($options);
+		$this->clock = $clock;
 
 		if (!empty($options['auth_password'])) {
 			$auth = new SimpleAuthenticator($options['auth_password']);
-			clock()->setAuthenticator($auth);
+			$clock->setAuthenticator($auth);
 		}
 
 		// authentication route
-		$this->fw->route('GET|POST /__clockwork/auth', function ($f3, $args) {
-			$token = clock()->getAuthenticator()->attempt(['password'=>$f3->get('POST.password')]);
+		$this->fw->route('GET|POST /__clockwork/auth', function ($f3, $args) use ($clock) {
+			$token = $clock->getAuthenticator()->attempt(['password'=>$f3->get('POST.password')]);
 			header('Content-Type: application/json');
 			$f3->status($token ? 200 : 403);
 			echo json_encode(['token'=>$token]);
@@ -46,63 +57,61 @@ class Debugger extends \Prefab {
 		});
 
 		if ($this->fw->exists('HEADERS.X-Clockwork-Auth',$token)) {
-
-			$authenticated = clock()->getAuthenticator()->check($token);
+			// metadata request from browser plugin
+			$authenticated = $clock->getAuthenticator()->check($token);
 			if ($authenticated === true) {
-				// skip tracking requests from browser plugin itself so only
-				// register route for receiving meta data
-				$this->fw->route('GET /__clockwork/*', function ($f3, $args) {
-					return clock()->returnMetadata( $args['*'] );
+				$this->fw->route('GET /__clockwork/*', function ($f3, $args) use ($clock)  {
+					return $clock->returnMetadata( $args['*'] );
 				});
-
 			} else {
 				header('Content-Type: application/json');
 				$this->fw->status(403);
-				echo json_encode(['message'=>$authenticated,'requires' => clock()->getAuthenticator()->requires()]);
+				echo json_encode(['message'=>$authenticated,'requires' => $clock->getAuthenticator()->requires()]);
 				exit();
 			}
 
 		} else {
+			// setup tracking requests
 
 			$this->fw->set('QUIET',TRUE);
-			clock()->startEvent('init',"App Init");
+			$clock->startEvent('init',"App Init");
 
-			$this->_parent->on('beforeLoad',function() {
-				clock()->endEvent('init');
+			$this->_parent->on('beforeLoad',function() use ($clock)  {
+				$clock->endEvent('init');
 
-				clock()->startEvent('app_load',"App Load");
+				$clock->startEvent('app_load',"App Load");
 			});
-			$this->_parent->on('beforeRun',function() {
-				clock()->endEvent('app_load');
+			$this->_parent->on('beforeRun',function() use ($clock)  {
+				$clock->endEvent('app_load');
 				$this->logHive();
-				clock()->startEvent('app',"App Run");
+				$clock->startEvent('app',"App Run");
 			});
 
-			$this->ev->on('route.call',function($args,$context,$ev) {
-				clock()->startEvent('route.call',$args);
+			$this->ev->on('route.call',function($args,$context,$ev) use ($clock)  {
+				$clock->startEvent('route.call',$args);
 			});
 
-			$this->ev->on('component.load',function($args,$context,$ev) {
-				clock()->startEvent('component.load.'.spl_object_hash($context),
+			$this->ev->on('component.load',function($args,$context,$ev) use ($clock)  {
+				$clock->startEvent('component.load.'.spl_object_hash($context),
 					'load: '.$args['name']);
 			});
 
-			$this->ev->on('component.ready',function($args,$context,$ev) {
-				clock()->endEvent('component.load.'.spl_object_hash($context));
-				clock()->info('component.ready: '.$args['name'],$args);
+			$this->ev->on('component.ready',function($args,$context,$ev) use ($clock)  {
+				$clock->endEvent('component.load.'.spl_object_hash($context));
+				$clock->info('component.ready: '.$args['name'],$args);
 			});
 
-			$this->ev->on('component.port.open',function($args,$context,$ev) {
-				clock()->startEvent('component.'.spl_object_hash($context).'.port.'.
+			$this->ev->on('component.port.open',function($args,$context,$ev) use ($clock)  {
+				$clock->startEvent('component.'.spl_object_hash($context).'.port.'.
 					$args['port'],'port: '.$args['name'].".".$args['port']);
 			});
 
-			$this->ev->on('component.port.close',function($args,$context,$ev) {
-				clock()->endEvent('component.'.spl_object_hash($context).'.port.'.
+			$this->ev->on('component.port.close',function($args,$context,$ev) use ($clock)  {
+				$clock->endEvent('component.'.spl_object_hash($context).'.port.'.
 					$args['port']);
 			});
 
-			$this->_parent->on('afterRun',function() {
+			$this->_parent->on('afterRun',function() use ($clock)  {
 				/** @var \Base $f3 */
 				$f3=\Base::instance();
 				$obj=Storage::instance()->get('sql');
@@ -114,12 +123,11 @@ class Debugger extends \Prefab {
 						if (preg_match('/\((\d+\.\d+ms)\)/',$ex[0].')',$match)) {
 							$time=((float)$match[1]);
 							$query=trim($ex[1]);
-							clock()->addDatabaseQuery($query,[],$time);
+							$clock->addDatabaseQuery($query,[],$time);
 						}
 					}
 				}
-
-				clock()->requestProcessed();
+				$clock->requestProcessed();
 				echo $f3->RESPONSE;
 			});
 		}
@@ -134,18 +142,20 @@ class Debugger extends \Prefab {
 //		}
 //		ksort($out);
 //		$hive->table('HIVE',$out);
-
-		clock()->info('HIVE',\Base::instance()->hive());
+		if ($this->enabled)
+			$this->clock->info('HIVE',\Base::instance()->hive());
 	}
 
 	function logContext($args,$context,$ev) {
 		if (!is_array($context))
 			$context = [$context];
-		clock()->info($ev['key'],$context);
+		if ($this->enabled)
+			$this->clock->info($ev['key'],$context);
 	}
 
 	function logArgs($args,$context,$ev) {
-		clock()->info($ev['key'],$args);
+		if ($this->enabled)
+			$this->clock->info($ev['key'],$args);
 	}
 
 }
